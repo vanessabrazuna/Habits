340bff5e8bb848bd78225f0bc0976bd3caa37bf2
vanessabrazuna <brazuna.nessa@gmail.com>
2025-09-17 19:13:55 -0300

    Structure layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
-import { Habit } from "./components/Habit"
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
 
 export function App() {
   return (
-    <div className="bg-emerald-500 text-white text-6xl font-bold rounded-lg w-20 h-20 p-6 mx-auto flex items-center justify-center mt-72 hover:bg-emerald-400 transition-colors cursor-pointer shadow-lg">
-      <Habit completed={3} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+
+        <SummaryTable />
+      </div>
     </div>
   )
 }
